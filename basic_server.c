ae7400e7f6283226227e5e41ac34a576f4a42117
TakingTheL <yliu6@stuy.edu>
2017-12-13 15:34:50 -0500

    we should be good, right???

--- basic_server.c
+++ basic_server.c
@@ -11,10 +11,10 @@ int main() {
 
   from_client = server_handshake( &to_client );
 
-  char[BUFFER_SIZE] msg_from_client;
+  char msg_from_client[BUFFER_SIZE];
   read(from_client, msg_from_client, BUFFER_SIZE);
 
-  char[BUFFER-SIZE] msg_to_client;
+  char msg_to_client[BUFFER-SIZE];
   msg_to_client = do_stuff(msg_from_client);
 
   write(to_client, msg_to_client, BUFFER_SIZE);
