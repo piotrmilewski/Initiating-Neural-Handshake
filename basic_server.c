61760eb73196e318cae098953015e68b08ff258f
TakingTheL <yliu6@stuy.edu>
2017-12-13 15:41:04 -0500

    wat

--- basic_server.c
+++ basic_server.c
@@ -13,11 +13,8 @@ int main() {
 
   from_client = server_handshake( &to_client );
 
-  char msg_from_client[BUFFER_SIZE];
-  read(from_client, msg_from_client, BUFFER_SIZE);
+  char buf[BUFFER_SIZE];
+  read(from_client, buf, BUFFER_SIZE);
 
-  char msg_to_client[BUFFER_SIZE];
-  sprintf(msg_to_client, "%s", msg_from_client);
-
-  write(to_client, msg_to_client, BUFFER_SIZE);
+  write(to_client, buf, BUFFER_SIZE);
 }
