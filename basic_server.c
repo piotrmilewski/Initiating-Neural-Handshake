e892ea7c08a67202c87688afe8896d228157d318
TakingTheL <yliu6@stuy.edu>
2017-12-13 15:36:06 -0500

    we weren't good :'(

--- basic_server.c
+++ basic_server.c
@@ -14,7 +14,7 @@ int main() {
   char msg_from_client[BUFFER_SIZE];
   read(from_client, msg_from_client, BUFFER_SIZE);
 
-  char msg_to_client[BUFFER-SIZE];
+  char msg_to_client[BUFFER_SIZE];
   msg_to_client = do_stuff(msg_from_client);
 
   write(to_client, msg_to_client, BUFFER_SIZE);
