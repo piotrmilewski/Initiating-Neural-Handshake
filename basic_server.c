c84fd159cccd2a535fdb1eedf7c31f77cdff0eca
TakingTheL <yliu6@stuy.edu>
2017-12-13 15:30:11 -0500

    server should be fine

--- basic_server.c
+++ basic_server.c
@@ -7,4 +7,12 @@ int main() {
   int from_client;
 
   from_client = server_handshake( &to_client );
+
+  char[BUFFER_SIZE] msg_from_client;
+  read(from_client, msg_from_client, BUFFER_SIZE);
+
+  char[BUFFER-SIZE] msg_to_client;
+  msg_to_client = do_stuff(msg_from_client);
+
+  write(to_client, msg_to_client, BUFFER_SIZE);
 }
